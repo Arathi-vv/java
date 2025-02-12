ee6efda43cb3993e4453f497a1e28657ca3e42d6
Arathi-vv <arathivv8086@gmail.com>
2025-02-12 10:55:43 +0530

    Add files via upload

---
+++ add2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class add2{
+public static void main(String[] args){
+ Scanner sc=new Scanner(System.in);
+ int a,b,sum;
+ System.out.println("Enter 2 numbers:");
+ a=sc.nextInt();
+ b=sc.nextInt();
+ sum=a+b;
+ System.out.println("The sum is:"+sum);}}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
