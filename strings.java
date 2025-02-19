5f39fc3303ac13fd2a8a56c85174557f1f73751f
Arathi-vv <arathivv8086@gmail.com>
2025-02-19 11:02:59 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,11 @@
+public class strings{
+   public static void main(String[] args){
+      String msg="Hello World";
+      System.out.println("The String is:"+msg);    
+      System.out.println(""+msg.length());    
+      System.out.println(msg.toLowerCase());
+      System.out.println(msg.toUpperCase());
+      String ms=" Welcome";
+      System.out.println(msg.concat(ms));
+    
+ }}
