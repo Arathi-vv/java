3c9d370c30c81ad567be4afcd648afad8fa3bb8c
Arathi-vv <arathivv8086@gmail.com>
2025-02-19 11:03:42 +0530

    Add files via upload

---
+++ sample_Sarray.java
@@ -0,0 +1,10 @@
+public class sample_Sarray{
+ public static void main(String[] args){
+  String[] fr={"apple","banana","orange"};
+  for(int i=0;i<3;i++){
+  System.out.println(fr[i]);
+  }
+  
+  
+  
+}}
