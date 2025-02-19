4f49e34bab15256c0cef282ce87b39d91983c207
Arathi-vv <arathivv8086@gmail.com>
2025-02-19 09:40:53 +0530

    Add files via upload

---
+++ loops_dowhile_helloWorld.java
@@ -0,0 +1,10 @@
+public class loops_dowhile_helloWorld
+{
+ public static void main(String[] args){
+   int i=1;
+   do{
+    System.out.println("Hello WorldS");
+    i++;
+    }
+   while(i<=5);
+    }}
