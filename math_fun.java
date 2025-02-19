6bcb2717c45662dcd9ecbbcee29f427161db4be9
Arathi-vv <arathivv8086@gmail.com>
2025-02-19 11:03:22 +0530

    Add files via upload

---
+++ math_fun.java
@@ -0,0 +1,10 @@
+public class math_fun{
+   public static void main(String[] args){
+    System.out.println("The 2 values 10,20");
+    System.out.println("Maximum value is:"+Math.max(10,20));
+    System.out.println("Minimum value is:"+Math.min(10,20));
+    System.out.println("Square root of 9:"+Math.sqrt(9));
+    System.out.println("Absolute value of -5:"+Math.abs(-5));
+    System.out.println("Random number(0-1):"+Math.random());
+    System.out.println("Random number(1000):"+Math.random()*1000);
+ }}
